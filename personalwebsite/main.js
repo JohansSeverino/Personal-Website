06f62d8d2f8f109428aa96641b1b638b3af4f3cd
JohansSeverino <johanss21@hotmail.com>
2019-08-02 15:28:06 -0400

    Personal website

---
+++ personalwebsite/main.js
@@ -0,0 +1,12 @@
+// $(document).ready(function() {
+
+//     if ((screen.width>700)) {
+//         // if screen size is 1025px wide or larger
+//         $(".burg").css('display', 'none'); // you can also use $(".yourClass").hide();
+//     }
+//     else if (screen.width<=700) {
+//         // if screen size width is less than 1024px
+//         $(".burg").css('display', 'block'); // here you can also use show();
+//     }
+
+//     });
